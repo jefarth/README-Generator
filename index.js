72ab027dfe647bd4a07c69b67724fb00fecc18ba
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-06-27 23:36:41 -0600

    Updated comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const generator = require(`./utils/generateMarkdown`)
 const inquirer = require(`inquirer`);
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -61,7 +61,7 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
 
 fs.writeFile("./"+fileName, data, function(err) {
@@ -73,7 +73,7 @@ fs.writeFile("./"+fileName, data, function(err) {
 
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
    inquirer.prompt(questions)
    .then(function(data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.Title}
   [![MIT Licence](https://badges.frapsoft.com/os/mit/mit.png?v=103)](https://opensource.org/licenses/mit-license.php)
