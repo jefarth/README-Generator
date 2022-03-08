eb42071f18adf179108bf370c666f169fdd0bbbb
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-03-07 22:17:30 -0700

    updated file readability

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,36 +4,34 @@ function generateMarkdown(data) {
   [![MIT Licence](https://badges.frapsoft.com/os/mit/mit.png?v=103)](https://opensource.org/licenses/mit-license.php)
 
   ## Description
-${data.Description}
+  ${data.Description}
 
-## Table of Contents 
+  ## Table of Contents 
 
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributions](#contributions)
-* [Tests](#tests)
-* [Questions](#questions)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
 
-## Installation
-Follow these steps to install the application: ${data.Installation}
+  ## Installation
+  Follow these steps to install the application: ${data.Installation}
 
-## Usage
-To use this application: ${data.Usage}
+  ## Usage
+  To use this application: ${data.Usage}
 
-## License
-This project is licensed under: ${data.License}.
+  ## License
+  This project is licensed under: ${data.License}.
 
-## Contributions
-Contributions made by: ${data.Contributions}
+  ## Contributions
+  Contributions made by: ${data.Contributions}
 
-## Tests
-Testing on this application was performed by: ${data.Test}
+  ## Tests
+  Testing on this application was performed by: ${data.Test}
 
-## Questions
-If you have any questions about the application, please contact "${data.UserName}" directly at ${data.Email}.
-
-`;
+  ## Questions
+  If you have any questions about the application, please contact "${data.UserName}" directly at ${data.Email}`;
 }
 
 module.exports = generateMarkdown;
